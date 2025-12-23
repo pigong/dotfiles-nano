701b22d1ab5f3088703add9f57775c21e65dbac2
Pigong <pi@igong.de>
2025-12-24 00:50:55 +0100

    Quick backup

---
+++ pigong.aliases.sh
@@ -0,0 +1,10 @@
+alias tp='trash-put'
+alias rm='echo "Use tp instead of rm!"'
+alias ..b='source ~/.bashrc'
+
+alias vi='/usr/bin/vim'
+alias vim='/usr/bin/nvim'
+
+alias ..='cd ..'
+alias ...='cd ../..'
+
